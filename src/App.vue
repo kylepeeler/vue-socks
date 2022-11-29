8b8d0b0d796a391ac76dae87d25648549a5d61df
Kyle Peeler <kpeeler@apple.com>
2022-11-29 16:21:24 -0500

    examples of using v-if and v-for

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
+import socksBlueImage from "./assets/images/socks_blue.jpeg";
 
 const product = ref("Socks");
+const image = ref(socksGreenImage);
+const inStock = ref(true);
+const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+const variants = ref([
+  { id: 2234, color: "green", image: socksGreenImage },
+  { id: 2235, color: "blue", image: socksBlueImage },
+]);
+
+const otherProduct = reactive({
+  name: "Shoes",
+});
+
+setTimeout(() => {
+  product.value = "New Socks";
+  otherProduct.name = "New Shoes";
+}, 1000);
 </script>
 
 <template>
-  <h1>{{ product }}</h1>
+  <div class="nav-bar"></div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image" />
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details" :key="detail">{{ detail }}</li>
+        </ul>
+        <div v-for="variant in variants" :key="variant.id">
+          {{ variant.color }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
